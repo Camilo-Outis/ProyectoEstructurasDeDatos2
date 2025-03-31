ac59f68e29fdf04cbba80c5e2aef8799f5264d48
Camilo-Outis <camiloumana34@gmail.com>
2025-03-30 18:41:11 -0600

    Cambios clase estadio

--- ProyectoEstructurasDeDatos/Src/Estadio.cpp
+++ ProyectoEstructurasDeDatos/Src/Estadio.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+
+
+class Estadio {
+public:
+    string nombre;
+    string ciudad;
+    string estado;
+    string pais;
+    int capacidad;
+    int anio;
+
+    Estadio(string nombre, string ciudad, string estado, string pais, int capacidad, int anio) {
+        this->nombre = nombre;
+        this->ciudad = ciudad;
+        this->estado = estado;
+        this->pais = pais;
+        this->capacidad = capacidad;
+        this->anio = anio;
+    }
+
+    string getNombre() { return nombre; }
+    string getPais() { return pais; }
+    int getCapacidad() { return capacidad; }
+    string toString() {
+        return nombre + ", " + ciudad + ", " + estado + ", " + pais + ", Capacidad: " + to_string(capacidad) + ", Año: " + to_string(anio);
+    }
+};
